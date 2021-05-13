32c4b0312b32e9fbb6b4cd02e049e1b4d7e41be3
Mayuran <mayuranswee23@gmail.com>
2021-05-13 00:18:01 -0400

    add ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,96 @@
+// var formEl = document.querySelector("#task-form");
+// var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+
+// var taskFormHandler = function (event) {
+
+//     event.preventDefault ();
+//     var taskNameInput = document.querySelector("input[name='task-name']").value;
+//     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+//     //check if input values are empty strings
+//     if (!taskNameInput || !taskTypeInput){
+//         alert ("You need to fill out the task form!");
+//         return false;
+//     }
+// formEl.reset();
+
+//  // package data as an object
+//  var taskDataObj = {
+//      name: taskNameInput, 
+//      type: taskTypeInput
+//  };
+
+//  //send it as an arguement
+//  createTaskEl(taskDataObj);
+// };
+
+
+// var createTaskEl = function(taskDataObj){
+// // create list item
+// var listItemEl = document.createElement("li");
+// listItemEl.className = "task-item";
+
+// // create div to hold task info and add to list item
+// var taskInfoEl = document.createElement("div");
+// taskInfoEl.className = "task-info";
+// taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
+// listItemEl.appendChild(taskInfoEl);
+
+// // add entire list item to list
+// tasksToDoEl.appendChild(listItemEl);
+// }
+
+
+
+
+
+// formEl.addEventListener("submit", taskFormHandler);
+
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+var taskFormHandler = function(event) {
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name'").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-var createFormHandler = function (event) {
+  // check if inputs are empty (validate)
+  if (taskNameInput === "" || taskTypeInput === "") {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  
+  formEl.reset();
 
-    event.preventDefault ();
-    var taskNameInput = document.querySelector("input[name='task-name']").value;
-    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  // reset form fields for next task to be entered
+  document.querySelector("input[name='task-name']").value = "";
+  document.querySelector("select[name='task-type']").selectedIndex = 0;
 
- // package data as an object
- var taskDataObj = {
-     name: taskNameInput, 
-     type: taskTypeInput
- };
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
 
- //send it as an arguement
- createTaskEl(taskDataObj);
+  createTaskEl(taskDataObj);
 };
 
+var createTaskEl = function(taskDataObj) {
+  // create list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
 
-var createTaskEl = function(taskDataObj){
-// create list item
-var listItemEl = document.createElement("li");
-listItemEl.className = "task-item";
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+  listItemEl.appendChild(taskInfoEl);
 
-// create div to hold task info and add to list item
-var taskInfoEl = document.createElement("div");
-taskInfoEl.className = "task-info";
-taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+  console.dir(listItemEl);
 
-listItemEl.appendChild(taskInfoEl);
+  // add list item to list
+  tasksToDoEl.appendChild(listItemEl);
+};
 
-// add entire list item to list
-tasksToDoEl.appendChild(listItemEl);
-}
-
-
-
-
-
-formEl.addEventListener("submit", createFormHandler);
-
+formEl.addEventListener("submit", taskFormHandler);
