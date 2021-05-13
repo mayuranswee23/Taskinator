9a90a0e806695e103556aaebfd3ffcb4fa1b6cb0
Mayuran <mayuranswee23@gmail.com>
2021-05-12 23:21:44 -0400

    add type to list

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,25 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function (event) {
 
     event.preventDefault ();
-  
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  // create list items 
     var listItemEl = document.createElement("li");
    listItemEl.className = "task-item";
-   listItemEl.textContent = "This is a new task.";
-   tasksToDoEl.appendChild(listItemEl); 
+
+   // create div to hold task info and add list items
+   var taskInfoEl = document.createElement ("div");
+   //give it a class name
+   taskInfoEl.className = "task-info";
+   // add HTML content to div
+   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+   listItemEl.appendChild(taskInfoEl);
+//    listItemEl.textContent = taskNameInput;
+//add entire list item to list
+    tasksToDoEl.appendChild(listItemEl); 
 };
 
 formEl.addEventListener("submit", createTaskHandler);
+
